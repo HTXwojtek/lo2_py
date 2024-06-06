a85085c16cc2ef017d6046d17609827902afaa3d
Akasso73 <michalnazwisko1473@gmail.com>
2024-06-06 19:30:18 +0200

    dodane nowe funkcje

--- Lekcje/Ciągi/rodzaje_ciagow.py
+++ Lekcje/Ciągi/rodzaje_ciagow.py
@@ -16,3 +16,20 @@ print(czy_geometryczny([3,9,27,81])) # True
 print(czy_arytmetyczny([3,9,27,81])) # False
 print(czy_geometryczny([3,6,9,12])) # False
 print(czy_arytmetyczny([3,6,9,12])) # True
+
+def czy_rosnacy(tab):
+    for i in range(1, len(tab)):
+        if tab[i] <= tab[i - 1]:
+            return False
+    return True
+
+def czy_malejacy(tab):
+    for i in range(1, len(tab)):
+        if tab[i] >= tab[i - 1]:
+            return False
+    return True
+
+print(czy_rosnacy([1,2,3,4])) # True
+print(czy_malejacy([1,2,3,4])) # False
+print(czy_malejacy([8,4,2,0])) # True
+print(czy_rosnacy([8,4,2,0])) # False
